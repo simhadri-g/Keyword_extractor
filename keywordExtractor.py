d9c216b7f3811f17bb2753b81adb2cdd1b1b08a0
Simhadri G <simhadri064@gmail.com>
2018-08-15 21:05:38 +0530

    Update keywordExtractor.py

--- keywordExtractor.py
+++ keywordExtractor.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Aug 13 19:55:26 2018
 
-@author: Simha
+@author: Simhadri
 """
 
 d = {}
@@ -12,7 +12,7 @@ threshold = int(input("Enter minimum threshold to be a keyword : "))
 filteredPassage = []
 for line in open(path):
     line = (line.lower())
-    print('start',line,'end of line')
+    #print('start',line,'end of line')
     for i in line:
         if (i<='z' and i >='a') or (i>='A' and i <='Z') :
             filteredPassage.append(i)
@@ -21,24 +21,24 @@ for line in open(path):
             filteredPassage.append(" ")
 strippedPassage = "".join([str(line) for line in filteredPassage])
 
-print(strippedPassage)
+#print(strippedPassage)
 
 
    
 strippedPassage = strippedPassage.split(' ')
 setPass =  set(strippedPassage) 
-print(setPass) 
+#print(setPass) 
 
 for word in setPass:
     if len(word)<=3 or word == 'with' or word == 'should' or word.startswith('th')== 1:
         pass
     else:
         d[word] = strippedPassage.count(word)
-print(d)       
-print(max(d.values()))
+#print(d)       
+#print(max(d.values()))
 arr = []
 for key in d:
-    print(key,'=>',d[key])       
+    #print(key,'=>',d[key])       
     if d[key]>threshold:
         arr.append(key)
 
